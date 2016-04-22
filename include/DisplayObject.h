ae48ea1b1ffd1f5d1d329835d6fbf55fed4839f2
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 22:11:50 +0200

    x y for display object

--- include/DisplayObject.h
+++ include/DisplayObject.h
@@ -6,6 +6,12 @@ namespace display {
     public:
         DisplayObject();
 
+        void setX(float value) { m_x = value; }
+        float x() { return m_x; }
+
+        void setY(float value) { m_y = value; }
+        float y() { return m_y; }
+
         void setWidth(float value) { m_width = value; }
         float width() { return m_width; }
 
@@ -16,8 +22,10 @@ namespace display {
         bool visible() { return m_visible; }
 
     private:
-        float m_width{0};
-        float m_height{0};
+        float m_x{0};
+        float m_y{0};
+        float m_width{1};
+        float m_height{1};
         bool m_visible{true};
     };
 
