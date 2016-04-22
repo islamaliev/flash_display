262aefe5462a3a1a43a2bbb7c6226a53bc141dbe
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 22:10:05 +0200

    DisplayObject visible property

--- include/DisplayObject.h
+++ include/DisplayObject.h
@@ -7,16 +7,18 @@ namespace display {
         DisplayObject();
 
         void setWidth(float value) { m_width = value; }
-
         float width() { return m_width; }
 
         void setHeight(float value) { m_height = value; }
-
         float height() { return m_height; }
+
+        void setVisible(bool value) { m_visible = value; }
+        bool visible() { return m_visible; }
 
     private:
         float m_width{0};
         float m_height{0};
+        bool m_visible{true};
     };
 
 }
