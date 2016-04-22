322d243ad52f233c8dc29f1735abacd29426674b
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 22:09:38 +0200

    Rectangle constructor

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -7,6 +7,15 @@ namespace core {
 
     class Rectangle {
     public:
+        Rectangle(float x, float y, float w, float h)
+            : m_x(x)
+            , m_y(y)
+            , m_width(w)
+            , m_height(h)
+        {}
+
+        Rectangle() = default;
+
         void setX(float value) { m_x = value; }
         float x() { return m_x; }
 
@@ -22,8 +31,8 @@ namespace core {
     private:
         float m_x{0};
         float m_y{0};
-        float m_width{0};
-        float m_height{0};
+        float m_width{1};
+        float m_height{1};
     };
 
 }
