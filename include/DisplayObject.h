66e91d566cffcec9245ba43e7247b6a24136cc14
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 22:29:27 +0200

    added pivotX, pivotY and rotation properties

--- include/DisplayObject.h
+++ include/DisplayObject.h
@@ -27,6 +27,15 @@ namespace display {
         void setVisible(bool value) { m_visible = value; }
         bool visible() { return m_visible; }
 
+        void setPivotX(float value) { m_pivotX = value; }
+        float pivotX() { return m_pivotX; }
+
+        void setPivotY(float value) { m_pivotY = value; }
+        float pivotY() { return m_pivotY; }
+
+        void setRotation(float value) { m_rotation = value; }
+        float rotation() { return m_rotation; }
+
     private:
         float m_x{0};
         float m_y{0};
@@ -34,6 +43,9 @@ namespace display {
         float m_height{1};
         float m_scaleX{1};
         float m_scaleY{1};
+        float m_pivotX{0};
+        float m_pivotY{0};
+        float m_rotation{0};
         bool m_visible{true};
     };
 
