4867afa34830d1b58b238a6c9e7184c845205ba2
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 21:47:39 +0200

    added Rectangle

---
+++ include/Rectangle.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+namespace flash {
+
+namespace core {
+
+    class Rectangle {
+    public:
+        void setX(float value) { m_x = value; }
+        float x() { return m_x; }
+
+        void setY(float value) { m_y = value; }
+        float y() { return m_y; }
+
+        void setWidth(float value) { m_width = value; }
+        float width() { return m_width; }
+
+        void setHeight(float value) { m_height = value; }
+        float height() { return m_height; }
+
+    private:
+        float m_x{0};
+        float m_y{0};
+        float m_width{0};
+        float m_height{0};
+    };
+
+}
+
+}
