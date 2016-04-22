9b0589942bf17721ee54794becf94aec03d85249
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 21:24:05 +0200

    added tests for width and height

--- include/DisplayObject.h
+++ include/DisplayObject.h
@@ -1,4 +1,14 @@
 class DisplayObject {
 public:
     DisplayObject();
+
+    void setWidth(float value) { m_width = value; }
+    float width() { return m_width; }
+
+    void setHeight(float value) { m_height = value; }
+    float height() { return m_height; }
+
+private:
+    float m_width{0};
+    float m_height{0};
 };
