ec8485e98fc1477d7bc8ccc1222d06b32a895ae3
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-23 13:15:51 +0200

    tested methods for adding and removing children

--- include/DisplayObjectContainer.h
+++ include/DisplayObjectContainer.h
@@ -2,6 +2,7 @@
 
 #include "DisplayObject.h"
 #include <cstddef>
+#include <vector>
 
 namespace flash {
 
@@ -9,18 +10,53 @@ namespace display {
 
     class DisplayObjectContainer : public DisplayObject {
     public:
-        std::size_t numChildren() { return m_numChildren; }
+        std::size_t numChildren() const { return m_children.size(); }
 
         void addChild(DisplayObject* child) {
-            m_numChildren++;
+            m_children.push_back(child);
+        }
+
+        void addChildAt(DisplayObject* child, std::size_t index) {
+            auto it = m_children.begin();
+            std::advance(it, index);
+            m_children.insert(it, child);
         }
 
         void removeChild(DisplayObject* child) {
-            m_numChildren--;
+            m_children.erase(std::remove(m_children.begin(), m_children.end(), child));
+        }
+
+        DisplayObject* removeChildAt(std::size_t index) {
+            auto it = m_children.begin();
+            std::advance(it, index);
+            DisplayObject* result = *it;
+            m_children.erase(it);
+            return result;
+        }
+
+        const DisplayObject* getChildAt(std::size_t index) const {
+            return m_children[index];
+        }
+
+        DisplayObject* getChildAt(std::size_t index) {
+            return m_children[index];
+        }
+
+        void removeChildren() {
+            m_children.clear();
+        }
+
+        std::size_t getChildIndex(DisplayObject* child) const {
+            auto beg = m_children.cbegin();
+            return std::find(beg, m_children.cend(), child) - beg;
+        }
+
+        bool contains(DisplayObject* child) const {
+            return std::find(m_children.cbegin(), m_children.cend(), child) != m_children.cend();
         }
 
     private:
-        std::size_t m_numChildren{0};
+        std::vector<DisplayObject*> m_children;
     };
 
 }
