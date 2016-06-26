ae472644cfe420f358df93a455c881bd5e4d6dcb
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-06-26 10:08:49 +0200

    moved some methods' implementations of DisplayObjectContainer to cpp file

--- include/DisplayObjectContainer.h
+++ include/DisplayObjectContainer.h
@@ -23,28 +23,14 @@ namespace display {
             addChildAt(child, m_children.size());
         }
 
-        void addChildAt(DisplayObject* child, std::size_t index) {
-            DisplayObjectContainer* parent = child->getParent();
-            if (parent) {
-                if (parent == this) {
-                    _moveChildTo(child, index);
-                    return;
-                }
-                // TODO: optimize so that when child is moved depth and tree size are calculated only once
-                parent->removeChild(child);
-            }
-            m_children.insert(m_children.begin() + index, child);
-            child->_setParent(this);
-            child->_updateDepth(depth());
-            _alterTreeSizeBy(child->treeSize());
-        }
+        void addChildAt(DisplayObject* child, std::size_t index);
 
         void removeChild(DisplayObject* child) {
-            _removeChildAt(std::find(m_children.begin(), m_children.end(), child));
+            _removeChildAt(std::find(m_children.cbegin(), m_children.cend(), child));
         }
 
         DisplayObject* removeChildAt(std::size_t index) {
-            return _removeChildAt(m_children.begin() + index);
+            return _removeChildAt(m_children.cbegin() + index);
         }
 
         const DisplayObject* getChildAt(std::size_t index) const {
@@ -55,22 +41,16 @@ namespace display {
             return m_children[index];
         }
 
-        void removeChildren() {
-            _alterTreeSizeBy(1 - treeSize());
-            for (auto& child : m_children) {
-                child->_resetDepth();
-                child->_setParent(nullptr);
-            }
-            m_children.clear();
-        }
+        void removeChildren();
 
         std::size_t getChildIndex(DisplayObject* child) const {
-            auto beg = m_children.cbegin();
+            const auto& beg = m_children.cbegin();
             return std::find(beg, m_children.cend(), child) - beg;
         }
 
         bool contains(DisplayObject* child) const {
-            return std::find(m_children.cbegin(), m_children.cend(), child) != m_children.cend();
+            const auto& end = m_children.cend();
+            return std::find(m_children.cbegin(), end, child) != end;
         }
 
         void draw(flash::render::Context&, flash::render::RenderState&) override;
@@ -82,49 +62,15 @@ namespace display {
         void setHeight(float value) override {};
 
     private:
-        void _resetDepth() override {
-            DisplayObject::_resetDepth();
-            for (auto& child : m_children) {
-                child->_resetDepth();
-            }
-        }
+        void _resetDepth() override;
 
-        void _updateDepth(int parentDepth) override {
-            DisplayObject::_updateDepth(parentDepth);
-            int d = depth();
-            for (auto& child : m_children) {
-                child->_updateDepth(d);
-            }
-        }
+        void _updateDepth(int parentDepth) override;
 
-        void _updateOrderIndex(int& orderIndex) override {
-            DisplayObject::_updateOrderIndex(orderIndex);
-            for (auto& child : m_children) {
-                child->_updateOrderIndex(++orderIndex);
-            }
-        }
+        void _updateOrderIndex(int& orderIndex) override;
 
-        DisplayObject* _removeChildAt(std::vector<DisplayObject*>::const_iterator it) {
-            DisplayObject* child = *it;
-            m_children.erase(it);
-            child->_setParent(nullptr);
-            child->_resetDepth();
-            _alterTreeSizeBy(-child->treeSize());
-            return child;
-        }
+        DisplayObject* _removeChildAt(std::vector<DisplayObject*>::const_iterator it);
 
-        void _moveChildTo(DisplayObject* child, int index) {
-            const auto& newIndex = m_children.begin() + index;
-            if (child == *newIndex || (index > 0 && child == *(newIndex - 1))) {
-                return;
-            }
-            const auto& currentIndex = std::find(m_children.begin(), m_children.end(), child);
-            bool moveLeft = currentIndex >= newIndex;
-            const auto& firstIt = moveLeft ? newIndex : currentIndex;
-            const auto& lastIt = !moveLeft ? newIndex : currentIndex + 1;
-            const auto& newFirstIt = moveLeft ? currentIndex : currentIndex + 1;
-            std::rotate(firstIt, newFirstIt, lastIt);
-        }
+        void _moveChildTo(DisplayObject* child, int index);
 
         std::vector<DisplayObject*> m_children;
 
--- src/DisplayObjectContainer.cpp
+++ src/DisplayObjectContainer.cpp
@@ -16,6 +16,75 @@ DisplayObjectContainer::DisplayObjectContainer()
     _setHeight(1);
 }
 
+void DisplayObjectContainer::addChildAt(DisplayObject* child, std::size_t index) {
+    DisplayObjectContainer* parent = child->getParent();
+    if (parent) {
+        if (parent == this) {
+            _moveChildTo(child, index);
+            return;
+        }
+        // TODO: optimize so that when child is moved depth and tree size are calculated only once
+        parent->removeChild(child);
+    }
+    m_children.insert(m_children.begin() + index, child);
+    child->_setParent(this);
+    child->_updateDepth(depth());
+    _alterTreeSizeBy(child->treeSize());
+}
+
+void DisplayObjectContainer::removeChildren() {
+    _alterTreeSizeBy(1 - treeSize());
+    for (auto& child : m_children) {
+        child->_resetDepth();
+        child->_setParent(nullptr);
+    }
+    m_children.clear();
+}
+
+void DisplayObjectContainer::_resetDepth() {
+    DisplayObject::_resetDepth();
+    for (auto& child : m_children) {
+        child->_resetDepth();
+    }
+}
+
+void DisplayObjectContainer::_updateDepth(int parentDepth) {
+    DisplayObject::_updateDepth(parentDepth);
+    int d = depth();
+    for (auto& child : m_children) {
+        child->_updateDepth(d);
+    }
+}
+
+void DisplayObjectContainer::_updateOrderIndex(int& orderIndex) {
+    DisplayObject::_updateOrderIndex(orderIndex);
+    for (auto& child : m_children) {
+        child->_updateOrderIndex(++orderIndex);
+    }
+}
+
+DisplayObject* DisplayObjectContainer::_removeChildAt(std::vector<DisplayObject*>::const_iterator it) {
+    DisplayObject* child = *it;
+    m_children.erase(it);
+    child->_setParent(nullptr);
+    child->_resetDepth();
+    _alterTreeSizeBy(-child->treeSize());
+    return child;
+}
+
+void DisplayObjectContainer::_moveChildTo(DisplayObject* child, int index) {
+    const auto& newIndex = m_children.begin() + index;
+    if (child == *newIndex || (index > 0 && child == *(newIndex - 1))) {
+        return;
+    }
+    const auto& currentIndex = std::find(m_children.begin(), m_children.end(), child);
+    bool moveLeft = currentIndex >= newIndex;
+    const auto& firstIt = moveLeft ? newIndex : currentIndex;
+    const auto& lastIt = !moveLeft ? newIndex : currentIndex + 1;
+    const auto& newFirstIt = moveLeft ? currentIndex : currentIndex + 1;
+    std::rotate(firstIt, newFirstIt, lastIt);
+}
+
 void DisplayObjectContainer::draw(Context& context, RenderState& renderState) {
     DisplayObject::_updateOrderIndex(renderState.orderIndex);
     for (auto child : m_children) {
